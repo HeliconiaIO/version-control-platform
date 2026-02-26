603da8345dd9770b35e84314c074a56cacf2befb
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-02-26 01:21:05 +0100

    [IMP] vcp_odoo:
    use new image.mixin, simplifying code base
    
    delete a repository branch should unlink related odoo modules. Otherwise, if we recreate after a repository branch, it will create odoo modules duplicated
    
    Display number of versions in which the module is available. By default, set a filter to hide modules without versions
    
    display all the versionsw where the module is available
    
    create a python file per model, following OCA convention
    
    entry menu item for vcp.odoo.module.version
    
    set readonly on all 'vcp.odoo.xxx' model fields
    
    rename vcp.odoo.lib.python into vcp.odoo.python.library. (following similar logic with the modele bin.package)
    
    add two new menu entries for vcp.odoo.python.library and vcp.odoo.bin.package
    
    display on python.library and bin.package form views, the list of odoo.module.version that uses those libs (or bins)
    
    Allow to unlink modules
    
    fetch html description
    
    harmonize actions. put a path in all actions. rename xml_id of some actions.

--- vcp_odoo/__manifest__.py
+++ vcp_odoo/__manifest__.py
@@ -11,9 +11,12 @@
     "depends": ["vcp_management"],
     "data": [
         "security/ir.model.access.csv",
-        "views/vcp_odoo_module_version.xml",
         "views/vcp_odoo_module.xml",
+        "views/vcp_odoo_module_version.xml",
+        "views/vcp_odoo_bin_package.xml",
+        "views/vcp_odoo_python_library.xml",
         "views/vcp_rule.xml",
+        "views/menu.xml",
         "data/vcp_rule.xml",
     ],
     "demo": [],
