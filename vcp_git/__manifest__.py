42927b9894efd9ce3322cdcd50a954a212d06ad8
Bhavesh Heliconia <oca@heliconia.in>
2026-06-29 10:44:18 +0530

    [MIG] vcp_git: Migration to 19.0

--- vcp_git/__manifest__.py
+++ vcp_git/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Vcp Git",
     "summary": """Allows to download code from git""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/version-control-platform",
